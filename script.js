d756603f8a36973bf1ac784e2332ef681bd4123c
swen9941-jpg <swen9941@gmail.com>
2025-11-30 13:54:35 -0600

    Initial commit: first interactive web page

---
+++ script.js
@@ -0,0 +1,3 @@
+document.getElementById("magicButton").addEventListener("click", function () {
+    document.getElementById("output").innerText = "🎉 You clicked the button! Computers are working for you!";
+});
